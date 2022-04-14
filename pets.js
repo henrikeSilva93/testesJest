5d9d714edf7915f0d5d50a373d45ee8ea67791ff
Aquila Henrique da Silva Faria <53267575+henrikeSilva93@users.noreply.github.com>
2022-04-14 16:07:01 -0300

    upload tests

---
+++ pets.js
@@ -0,0 +1,52 @@
+
+const data = [
+    { id: 1, nome: "Capitu", 
+        idade: 15, sexo: "F",
+        especie: "Calopsita"
+    },
+    {
+      id: 2, nome: "Bentinho",
+        idade: 5, sexo: "M",
+        especie: "Cachorro"
+    }
+];
+
+function listar() {
+    return data;
+}
+
+function listarPorId(id) {
+    const pet = data.find(pet => pet.id === id)
+    if(!pet) {
+        return {
+            erro: true,
+            mensagem: "Pet não encontrado!"
+        };
+    }
+    return pet;
+}
+
+function cadastrar(pet) {
+   pet.id = data.length+1;
+    data.push(pet);
+    return pet;
+}
+
+function editarPorId(id, pet) {
+    const index = data.findIndex(pet => pet.id === id);
+    if(index === -1)
+        return {erro: true, mensagem: "Pet não encontrado!"}
+}
+
+function deletarPorId(id) {
+    const index = data.findIndex(pet => pet.id === id);
+    if(index === -1) return {erro: true, mensagem: "Pet não encontrado!"}
+}
+
+module.exports = {
+    listar,
+    listarPorId,
+    cadastrar,
+    editarPorId,
+    deletarPorId
+}
